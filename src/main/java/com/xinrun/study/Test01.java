f2e77e30f11563c0ce65c35774fd6410123d7388
Wahaha4539 <1052234105@qq.com>
2017-08-06 18:53:04 +0800

    Test01

---
+++ src/main/java/com/xinrun/study/Test01.java
@@ -0,0 +1,10 @@
+package com.xinrun.study;
+
+/**
+ * Created by 1 on 2017/8/6.
+ */
+public class Test01 {
+    public static void mian(String[]args){
+        System.out.println("I love JAVA!");
+    }
+}
